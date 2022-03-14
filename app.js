40ea919910ab035dfe82021490beb5f71a20b79d
gwht915 <gwht915@gmail.com>
2022-03-14 11:44:23 +0800

    feat: add.js updated(drop the extra db connection)

--- app.js
+++ app.js
@@ -32,18 +32,18 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+//mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
-const db = mongoose.connection
+//const db = mongoose.connection
 // 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected(db Open)!')
-})
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
+// // 連線成功
+// db.once('open', () => {
+//   console.log('mongodb connected(db Open)!')
+// })
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
